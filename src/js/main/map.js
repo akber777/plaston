e7eb92f80283040a1323aeacad345089db0404c2
Akber Akhmedzadeh <akber_akhmedzadeh@mail.ru>
2020-05-24 00:17:14 +0400

    start

---
+++ src/js/main/map.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    
+    ymaps.ready(init);
+
+    function init() {
+        var myMap = new ymaps.Map("map", {
+            center: [42.436688, 18.5958266],
+            zoom: 10,
+            controls: []
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+            myPlacemark1 = new ymaps.Placemark([42.436688, 18.59582661], {
+                balloonContent: 'Large icon'
+            }, {
+                iconLayout: 'default#image',
+                // iconImageClipRect: [[69,0], [97, 46]],
+                iconImageHref: 'img/placemarker.png',
+                iconImageSize: [70, 76],
+                iconImageOffset: [-35, -63]
+            });
+
+        myMap.geoObjects
+            .add(myPlacemark1);
+    }
+
+
+
+
+
+})
---
+++ src/js/main/slider.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+
+    $('.homeOwl').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+
+
+
+    $('.mynewowl').owlCarousel({
+        autoplay: true,
+        center: true,
+        loop: true,
+        nav: true,
+        margin: 0,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+
+
+})
+
